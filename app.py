36f3d84ec8a2cbbe66e8a45a8a09e14ea5dbbf93
OmarElsebaey001 <60271793+OmarElsebaey001@users.noreply.github.com>
2021-07-18 04:30:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(os.eniron.get('MONGODB_URI'))
+    client = MongoClient(os.environ.get('MONGODB_URI'))
     app.db = client.data_collection_db
 
     @app.route('/',methods=['GET','POST'])
